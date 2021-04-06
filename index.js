b9d39be887812cb171cab95343dea8171364b100
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-06 10:24:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "0px";
 
 document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
+
+document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
