1aa3bfea1d1e1bf081eb15df0cced8e1a34c1970
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-06 10:30:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,12 @@ document.addEventListener("keydown", function(e) {
   }
 });
 
-let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
