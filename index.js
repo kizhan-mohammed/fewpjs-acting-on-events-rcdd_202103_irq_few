1f20c98fd67cf8f95e4765a690b6cc8f5fdc0db1
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-06 10:25:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "0px";
 
-document.addEventListener("keydown", function(e) {
-  console.log(e.key);
-});
+
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
